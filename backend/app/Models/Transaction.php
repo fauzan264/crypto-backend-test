ad90d46337ee17fa3d556da1c12c9d23e3f6863f
Fauzan <fauzan.c4@gmail.com>
2025-02-07 17:41:12 +0700

    update query GetDepositsByMonth

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Number;
+use NumberFormatter;
 
 class Transaction extends Model
 {
@@ -23,10 +25,13 @@ class Transaction extends Model
 
     public static function GetDepositsByMonth($month, $year)
     {
-        $query = self::selectRaw("YEAR(created_at) as year, MONTH(created_at) as month, COUNT(id) as total")
+        $query = self::selectRaw("MONTH(created_at) as month, YEAR(created_at) as year, SUM(amount) as total")
                     ->whereRaw("MONTH(created_at) = ? AND YEAR(created_at) = ? AND type = 'deposit'", [$month, $year])
                     ->groupByRaw("YEAR(created_at), MONTH(created_at)")
-                    ->get();
+                    ->first();
+
+        $formatter = new NumberFormatter('id_ID', NumberFormatter::CURRENCY);
+        $query->total = $formatter->formatCurrency($query->total, "IDR");
 
         return $query;
     }
