0265470cb2961b76dab9535792b1df51acef9164
Fauzan <fauzan.c4@gmail.com>
2025-02-07 16:29:26 +0700

    update BaseResource file

--- backend/app/Http/Resources/BaseResource.php
+++ backend/app/Http/Resources/BaseResource.php
@@ -7,6 +7,25 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BaseResource extends JsonResource
 {
+    // define properties
+    public $status;
+    public $message;
+    public $resource;
+
+    /**
+     * __construct
+     * 
+     * @param mixed $status
+     * @param mixed $message
+     * @param mixed $resource
+     */
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status = $status;
+        $this->message = $message;
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -14,6 +33,10 @@ class BaseResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource,
+        ];
     }
 }
