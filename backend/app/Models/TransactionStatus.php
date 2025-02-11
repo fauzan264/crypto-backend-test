da35888bdb21a8870efa037c11c425876fd68521
Fauzan <fauzan.c4@gmail.com>
2025-02-11 11:22:45 +0700

    create endpoint create transaction sattus

--- backend/app/Models/TransactionStatus.php
+++ backend/app/Models/TransactionStatus.php
@@ -5,17 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Log;
 
 class TransactionStatus extends Model
 {
-    use HasUuids;
     protected $primarykey = "name";
     protected $keyType = "string";
     protected $table = "transaction_status";
+    public $incrementing = false;
     public $timestamps = true;
+    protected $fillable = ["name", "created_at", "created_by", "updated_at", "updated_by"];
 
     public function transaction(): BelongsTo
     {
         return $this->belongsTo("transaction_status", "status", "name");
+    }
+
+    public static function storePost($request)
+    {
+        return self::create([
+            'name'  => $request->name,
+            'created_by' => '00000000-0000-0000-0000-000000000000',
+        ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\StatsController;
+use App\Http\Controllers\Api\TransactionStatusController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +10,9 @@ Route::prefix('v1')->group(function() {
     Route::prefix('stats')->group(function() {
         Route::get('/registrations', [StatsController::class, 'registrations']);
         Route::get('/deposits', [StatsController::class, 'deposits']);
+    });
+
+    Route::prefix('transaction_status')->group(function() {
+        Route::post('/', [TransactionStatusController::class, 'store']);
     });
 });
